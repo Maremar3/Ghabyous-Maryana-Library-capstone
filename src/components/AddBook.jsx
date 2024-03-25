c758ed06f8cbce000e4a1e6fb416d9411bfc2de9
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-24 19:16:52 -0700

    ADD BOOK FORM WORKS AS A DESIGN WITHOUT FUNCTIONALTY

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,37 +1,106 @@
 import { Description } from '@mui/icons-material';
-import { FormLabel, TextField } from '@mui/material'
+import { Button, FormLabel, TextField, FormControlLabel, Checkbox } from '@mui/material'
+import "../components/Book/Book.css"
 import React, { useState } from 'react'
+// import box from 'react-dom'
 
 function AddBook() {
-  const  [inputs,setInputs]=useState({
-    name:'',
-    description:'',
-    price:'',
-    auther:'',
-    available:false,
-    image:''
+  const [inputs, setInputs] = useState({
+    name: '',
+    description: '',
+    price: '',
+    auther: '',
+    available: false,
+    image: ''
   });
-return(
-  <div></div>
+  const [checked, setChecked] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+    //console.log(e.target.name,"value",e.target.value)
 
-)
-  // return (
-  //   <div>
-  //     Add Books From Here
-  //   </div>
-  // )
+  }
+  return (
+    <div>
+      <form>
+        <box>
+
+
+          <FormLabel>Name</FormLabel>
+          <TextField value={inputs.name} onChange={handleChange} margin="normal" variant='outlined' name="name" />
+          <FormLabel>Auther</FormLabel>
+          <TextField margin="normal" variant='outlined' name="auther" />
+          <FormLabel>Price</FormLabel>
+          <TextField type='number' margin="normal" variant='outlined' name="price" />
+          <FormLabel>Description</FormLabel>
+          <TextField margin="normal" variant='outlined' name="description" />
+          <FormLabel>image</FormLabel>
+          <TextField margin="normal" variant='outlined' name="image" />
+          {/* <FormControlLabel control={<Checkbox checked={false} />} label="Label" /> */}
+          <FormControlLabel control={<Checkbox checked={checked} onChange={() => setChecked(!checked)} />} label="Label" />
+          <Button variant='contain' type='submit'>
+            Add Book
+          </Button>
+
+
+        </box>
+      </form>
+    </div>
+  )
+  // return
+
+
+  // <form>
+
+  // {/* <Box
+  //  display = "flex"
+  //   flexDirection="column"
+  //    justifyContent={'center'} 
+  //    maxWidth={700}
+  //   alignContent={"center"}
+  //   alignSelf="center"
+  //   marginLeft={"auto"}
+  //   marginRight="auto"
+  //   marginTop={10}
+  // > */}
+  // <FormLabel>Name</FormLabel>
+  // <TextField margin="normal" fullwidth variant='outlined' name="name"/>
+  //  {/* <FormLabel>Auther</FormLabel>
+  //   <TextField value={inputs.name} onChange={handleChange} margin="normal" fullwidth variant='outlined' name="name"/> */}
+  // <FormLabel>Auther</FormLabel>
+  // <TextField margin="normal" fullwidth variant='outlined' name="auther"/>
+  // {/* <FormLabel>Description</FormLabel>
+  //   <TextField value={inputs.description} onChange={handleChange} margin="normal" fullwidth variant='outlined' name="auther"/> */}
+  //  <FormLabel>Description</FormLabel>
+  // <TextField margin="normal" fullwidth variant='outlined' name="description"/>
+  //  {/* <FormLabel>Price</FormLabel>
+  //   <TextField value={inputs.price} onChange={handleChange} margin="normal" fullwidth variant='outlined' name="description"/> */}
+  // <FormLabel>Price</FormLabel>
+  // <TextField type="number" margin="normal" fullwidth variant='outlined' name="price"/>
+  // <FormLabel>image</FormLabel>
+  // <TextField  margin="normal" fullwidth variant='outlined' name="image"/>
+  // {/* <TextField value={inputs.image} onChange={handleChange} margin="normal" fullwidth variant='outlined' name="image"/> */}
+  //  {/* <FormControlLabel control={<Checkbox checked={inputs.available} />} label="available" /> */}
+  // {/* <Button variant="contained" type="submit">Add Book</Button> */}
+  //  {/* </Box> */}
+
+  // </form>
+
+
+
+
+
+  // {/* <form> */}
+
+  // {/* <FormLabel>Name</FormLabel> */}
+  // {/* <TextField  margin="normal" fullwidth variant='outlined' name="name" />  */}
+
+
+  // // </form>
+
 }
 
 export default AddBook
 
-// import React from 'react'
-
-// const AddBook = () => {
-//   return 
-//     <div>
-//       Add Books From Here
-//     </div>
-  
-// }
-
-// export default AddBook
--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -5,26 +5,18 @@ import "./Book.css"
 //import {Button} from 'react-native'
 import "./Book.css"
 function Book(props) {
-    const { _id, name, auther, price, image} = props.book;
+  const { _id, name, auther, price, image } = props.book;
 
   return (
-  
     <div className='card'>
-     
-
-     <form>
-
-     <img src={image}  alt={name}/>
-     <h3>{name}</h3>
-      <article>By {auther}</article>
-    
-      <h2>${price}</h2>
-      {/* <Button>Update</Button> */}
-      <Button variant="contained">Delete</Button>
-      <Button variant="outlined">Update</Button>
-      {/* <Button variant="contained">Update</Button> */}
+      <form>
+        <img src={image} alt={name} />
+        <h3>{name}</h3>
+        <article>By {auther}</article>
+        <h2>${price}</h2>
+        <Button variant="contained">Delete</Button>
+        <Button variant="outlined">Update</Button>
       </form>
-  
     </div>
   )
 }
